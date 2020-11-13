11116cebb89af0bd6cfcb792592a42b3bd3a5ff4
Wale Joseph <30349247+josoga2@users.noreply.github.com>
2020-11-13 21:20:37 +0300

    Add files via upload

---
+++ Fisher teext.R
@@ -0,0 +1,76 @@
+#4054-4068
+x1 = matrix(c(760,	30626,
+              5465,	820066
+), nrow = 2)
+fisher.test(x1)
+#4097-4126
+x2 = matrix(c(2244,	29142,
+              54548,	767762
+), nrow = 2)
+fisher.test(x2)
+#4139-4158
+x3 = matrix(c(2195,	29191,
+              69108,	753202
+), nrow = 2)
+fisher.test(x3)
+#4165-4191
+x4 = matrix(c(1548,	29838,
+              32887,	789423
+), nrow = 2)
+fisher.test(x4)
+#4255-4276
+x5 = matrix(c(1868,	29518,
+              45343,	776967
+), nrow = 2)
+fisher.test(x5)
+#4343-4359
+x6 = matrix(c(1976,	29410,
+              92055,	730255
+), nrow = 2)
+fisher.test(x6)
+#4606-4640
+x7 = matrix(c(3068,	28318,
+              103186,	719124
+), nrow = 2)
+fisher.test(x7)
+#4642-4658
+x8 = matrix(c(1757,	29629,
+              5320,	816990), nrow = 2)
+fisher.test(x8)
+#4668-4684
+x9 = matrix(c(2187,	29199,
+              53624,	768686
+), nrow = 2)
+fisher.test(x9)
+#4691-4722
+x10 = matrix(c(2305,	29081,
+               60871,	761439
+), nrow = 2)
+fisher.test(x10)
+#4722-4741
+x11 = matrix(c(2891,	28495,
+               69760,	752550
+), nrow = 2)
+fisher.test(x11)
+#4882-4941
+x12 = matrix(c(8587, 22799,
+               216520,	605790
+), nrow = 2)
+fisher.test(x12)
+#4385-4397
+x13 = matrix(c(0,	31386,
+               13623,	808687
+               
+), nrow = 2)
+fisher.test(x13)
+
+#4054-4068
+cis = matrix(c(1436,	31386,
+              1631,	822310
+), nrow = 2)
+fisher.test(cis)
+
+# imargi result
+imargi = matrix(c(29, 128,
+                  182, 271767), nrow = 2)
+fisher.test(imargi)
